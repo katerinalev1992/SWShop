c8d42014954fbb49fb3e88cd8e20594b469218f8
klevytska <klevytska@gk-software.com>
2016-12-08 15:48:40 +0200

    Added ApliccationScope annotation

--- src/main/java/com/klevytska/swshop/repository/CustomerRepository.java
+++ src/main/java/com/klevytska/swshop/repository/CustomerRepository.java
@@ -1,7 +1,10 @@
 package com.klevytska.swshop.repository;
 
+import javax.enterprise.context.ApplicationScoped;
+
 /**
  * Created by klevytska on 08.12.2016.
  */
+@ApplicationScoped
 public class CustomerRepository {
 }
